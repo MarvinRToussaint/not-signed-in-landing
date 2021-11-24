128ff909509e9c088eabdf7904b529ffa9db1418
MarvinRToussaint <mtoussaint2163@gmail.com>
2021-11-23 21:27:01 -0500

    initilizing project

---
+++ src/components/Landing.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import styled from "styled-components";
+
+function Landing() {
+  return (
+    <Container>
+      <h1>WebPAGE</h1>
+    </Container>
+  );
+}
+
+export default Landing;
+
+const Container = styled.div`
+  heigth: 100vh;
+`;
